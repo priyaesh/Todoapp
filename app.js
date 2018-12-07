7f66fbab817b40e14c56ff3891747eb84f4fd70f
unknown <hemapriyasampath@gmail.com>
2018-12-06 20:37:43 -0500

    read todo

---
+++ app.js
@@ -0,0 +1,68 @@
+const express = require('express');
+const app = express();
+const bodyParser = require("body-parser");
+const mongo = require('mongodb');
+const MongoClient= mongo.MongoClient;
+const ObjectId = require('mongodb').ObjectId;
+const url ="mongodb://127.0.0.1:27017/";
+
+
+app.use("/", express.static(__dirname +'/'));
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.json());
+
+app.post("/tasks/new",function(req,res){
+    
+    MongoClient.connect(url,function(err, mgClient){
+
+        if(err){ 
+            console.log("Error in connection");
+            console.log(err);
+        }else {
+            console.log("Connection successful ");
+            console.log("inside the post");
+            let db = mgClient.db("amazingtodoapp");
+            let tasks= db.collection('tasks');
+
+            tasks.insertOne({
+                timestamp:new Date(),
+                //description:"Esaa Test"
+                description:req.body.description
+            });
+
+        }
+        res.redirect("/");
+    })
+
+})
+// app.get("/tasks",function(req,res){
+//     MongoClient.connect(url,function(err,db){
+//         if(!err){
+//             let tasks=db.collection('tasks');
+//             tasks.find({}).toArray(function(err, results){
+//                 res.send(JSON.stringify(results));
+//             })
+//         }
+//     })
+// })
+
+// app.put("/tasks/update/:id",function(req,res){
+//     MongoClient.connect(url,function(err,db){
+//         let tasks =db.collection('tasks');
+//         tasks.update({
+//             _id: new ObjectId(req.params.id)
+//             },{$set:{
+//             description: req.body.description
+//             }}
+
+//         )
+//     })
+// })
+
+app.listen(3000,function(err){
+    if(err){
+        console.log("Error in connection"+ err);
+    } else {
+        console.log("Server ready");
+    }
+});
